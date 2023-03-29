ea353b0347e75c06a3a65c26c1888a96ddf83f7e
pythonpete32 <aaron@blackscale.io>
2023-03-29 15:16:17 +0400

    hardcode metadata for deployment

--- deploy/20_create_lens_voting_repo.ts
+++ deploy/20_create_lens_voting_repo.ts
@@ -11,16 +11,23 @@ const func: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
 
   const networkName = network.name === "local" ? "mainnet" : network.name;
 
+  const buildMetadataCid =
+    "bafybeifmtk6i3bqcn465j3adtf3iiietsyzqjdrahz7ftole7x2nlr2yca";
+  const releaseMetadataCid =
+    "bafybeihn5jqhaqawlioddpey4b5bbdffhbzxmncl5z7urwh6d5wmepm4pm";
+
   console.warn(
     `\n20: Creating lens-voting repo \nPlease make sure pluginRepo is not created more than once with the same subdomain.`
   );
 
   await newPluginRepo({
-    subdomain: "and-another-0ne",
+    subdomain: "lens",
     setupAddress,
     deployer,
     networkName,
     signer,
+    buildMetadataCid,
+    releaseMetadataCid,
   });
 };
 

c256c8aa44b0f24bcfc2199ac4a7014ee074ab7f
pythonpete32 <aaron@blackscale.io>
2023-04-20 13:45:22 +0400

    fix name clash

--- contracts/utils/Ratio.sol
+++ contracts/utils/Ratio.sol
@@ -8,7 +8,7 @@ uint256 constant RATIO_BASE = 10 ** 6;
 /// @notice Thrown if a ratio value exceeds the maximal value of `10**6`.
 /// @param limit The maximal value.
 /// @param actual The actual value.
-error RatioOutOfBounds(uint256 limit, uint256 actual);
+error RatioOutOfBound(uint256 limit, uint256 actual);
 
 /// @notice Applies a ratio to a value and ceils the remainder.
 /// @param _value The value to which the ratio is applied.
@@ -16,7 +16,7 @@ error RatioOutOfBounds(uint256 limit, uint256 actual);
 /// @return result The resulting value.
 function _applyRatioCeiled(uint256 _value, uint256 _ratio) pure returns (uint256 result) {
     if (_ratio > RATIO_BASE) {
-        revert RatioOutOfBounds({limit: RATIO_BASE, actual: _ratio});
+        revert RatioOutOfBound({limit: RATIO_BASE, actual: _ratio});
     }
 
     _value = _value * _ratio;
@@ -35,7 +35,7 @@ function _applyRatioCeiled(uint256 _value, uint256 _ratio) pure returns (uint256
 /// @return result The resulting value.
 function _applyRatioFloored(uint256 _value, uint256 _ratio) pure returns (uint256 result) {
     if (_ratio > RATIO_BASE) {
-        revert RatioOutOfBounds({limit: RATIO_BASE, actual: _ratio});
+        revert RatioOutOfBound({limit: RATIO_BASE, actual: _ratio});
     }
 
     result = (_value * _ratio) / RATIO_BASE;
